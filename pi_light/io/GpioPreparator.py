9ab916da46789631866d09a3918b690516734e2b
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-03-21 17:04:44 +0100

    Using switch class to prepare GPIO

---
+++ pi_light/io/GpioPreparator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Handling the GPIO interface for different buttons"""
+
+from pi_light import pi_light as main_module
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  Did you do sudo?")
+
+__author__ = "Chris Kittl"
+__version__ = "0.1"
+__license__ = "GPL v 3"
+__status__ = "Under Development"
+
+
+def prepare(switch_set):
+    main_module.logger.debug("Preparing GPIO")
+    GPIO.setmode(GPIO.BOARD)
+    for switch in switch_set:
+        GPIO.setup(switch.pin_channel, GPIO.IN)
+        GPIO.add_event_detect(switch.pin_channel, GPIO.FALLING, callback=main_module.button_pushed,
+                              bouncetime=switch.bounce_time)
+        main_module.logger.info("Setting switch \"%s\" to pin %i with bounce time %i ms", switch.id, switch.pin_channel,
+                                switch.bounce_time)
+    main_module.logger.debug("Ready preparing GPIO")
--- pi_light/pi_light.py
+++ pi_light/pi_light.py
@@ -5,7 +5,8 @@
 import logging.config
 import time
 import datetime
-import pi_light.io.Button as Button
+import pi_light.io.GpioPreparator as Button
+from pi_light.io.Switch import Switch
 
 __author__ = "Chris Kittl"
 __version__ = "0.1"
@@ -16,22 +17,20 @@ logging.config.fileConfig('pi_light/resources/logging.conf')
 logger = logging.getLogger("piLight")
 
 # Global variables
-set_pin = 11
-set_bounce_time = 10000  # bounce time for set pin (10s)
-reset_pin = 13
-reset_bounce_time = 1000  # bounce time for set pin (1s)
+set_switch = Switch("set", 11, 10000)
+reset_switch = Switch("reset", 13, 1000)
 
 
 def run():
     logger.info("Starting pi light")
-    Button.prepare(set_pin, set_bounce_time, reset_pin, reset_bounce_time)
+    Button.prepare({set_switch, reset_switch})
     while True:
         logger.debug("It's %s, waiting another second", datetime.datetime.now())
         time.sleep(1)
 
 
 def button_pushed(channel):
-    if channel == set_pin:
+    if channel == set_switch.pin_channel:
         logger.info("Set button has been pushed")
-    if channel == reset_pin:
+    if channel == reset_switch.pin_channel:
         logger.info("Reset button has been pushed")
